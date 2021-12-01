e643e67e0cad421878749b5c1f9136d0cb640551
Nick McIntyre <nick@mcintyre.io>
2021-11-30 20:27:06 -0600

    Add notebook assets

---
+++ notebook/build.py
@@ -0,0 +1,15 @@
+import os
+
+files = ['p5.js', 'p5.sound.min.js', 'math.js', 'p5.strive.js', 'skulpt.min.js', 'skulpt-stdlib.js', 'jquery-3.5.1.min.js', 'skulptSetup.js']
+
+package = ''
+
+for fn in files:
+  path = os.path.join(os.getcwd(), fn)
+  with open(path) as f:
+    for line in f:
+      package += line
+  package += '\n'
+
+with open('strive.js', 'w') as f:
+  f.write(package)
